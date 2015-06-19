df5e60c838cd81d9e53f6b221d12e2c1ae9a35ca
dinghao <dinghao@coralsec.com>
2015-06-19 17:57:31 +0800

    Revert "Revert "LinkOne用来生成LinkOne.framework,然后导入LinkMain的Document包里""
    This reverts commit 10fb44c962865e2fa4294e63f2d87926169b5951.

---
+++ LinkMain/LinkMain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkMain
+//
+//  Created by 小伍 on 15-6-17.
+//  Copyright (c) 2015年 小伍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
